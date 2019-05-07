4677a9e7dfc25f1469dea4df62482d3a00f32956
sale37 <sale-37@live.com>
2019-05-07 13:53:48 +0200

    Added method show movie.

--- app/Http/Controllers/Api/MovieController.php
+++ app/Http/Controllers/Api/MovieController.php
@@ -40,9 +40,9 @@ class MovieController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Movie $movie)
     {
-        //
+        return $movie;
     }
 
     /**
