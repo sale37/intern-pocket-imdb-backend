183c92a949d36c9b0ba39c903b9a5dd30070c24b
sale37 <sale-37@live.com>
2019-05-09 10:43:56 +0200

    Code refactoring.

--- database/migrations/2019_05_08_113402_alter_movies_users_table_drop_liked_disliked_columns.php
+++ database/migrations/2019_05_08_113402_alter_movies_users_table_drop_liked_disliked_columns.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMoviesUsersTable2 extends Migration
+class AlterMoviesUsersTableDropLikedDislikedColumns extends Migration
 {
     /**
      * Run the migrations.
